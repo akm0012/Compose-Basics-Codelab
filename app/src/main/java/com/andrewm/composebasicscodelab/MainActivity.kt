246cb61c334b767f70cfd3b47128a0fa0d8e1c15
Andrew Marshall <andrewm@twitter.com>
2022-06-29 19:48:06 -0400

    Added a start screen and some state variables

--- app/src/main/java/com/andrewm/composebasicscodelab/MainActivity.kt
+++ app/src/main/java/com/andrewm/composebasicscodelab/MainActivity.kt
@@ -3,16 +3,10 @@ package com.andrewm.composebasicscodelab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.OutlinedButton
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -31,7 +25,19 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-private fun MyApp(names: List<String> = listOf("World", "Android", "Everyone")) {
+private fun MyApp() {
+
+    var shouldShowOnboarding by remember { mutableStateOf(true) }
+
+    if (shouldShowOnboarding) {
+        OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false })
+    } else {
+        Greetings()
+    }
+}
+
+@Composable
+private fun Greetings(names: List<String> = listOf("World", "Android", "Everyone")) {
     Column(modifier = Modifier.padding(vertical = 4.dp)) {
         for (name in names) {
             Greeting(name = name)
@@ -42,8 +48,8 @@ private fun MyApp(names: List<String> = listOf("World", "Android", "Everyone")) 
 @Composable
 fun Greeting(name: String) {
 
-    val expanded = remember { mutableStateOf(false) }
-    val extraPadding = if (expanded.value) 48.dp else 0.dp
+    var expanded by remember { mutableStateOf(false) }
+    val extraPadding = if (expanded) 48.dp else 0.dp
 
     Surface(
         color = MaterialTheme.colors.primary,
@@ -62,8 +68,8 @@ fun Greeting(name: String) {
             }
 
             OutlinedButton(
-                onClick = { expanded.value = !expanded.value }) {
-                Text(text = if (expanded.value) "Show less" else "Show more")
+                onClick = { expanded = !expanded }) {
+                Text(text = if (expanded) "Show less" else "Show more")
             }
         }
 
@@ -75,5 +81,32 @@ fun Greeting(name: String) {
 fun DefaultPreview() {
     ComposeBasicsCodelabTheme {
         MyApp()
+    }
+}
+
+@Composable
+fun OnboardingScreen(onContinueClicked: () -> Unit) {
+    Surface {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text("Welcome to the Basics Codelab!")
+            Button(
+                modifier = Modifier.padding(vertical = 24.dp),
+                onClick =  onContinueClicked
+            ) {
+                Text("Continue")
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Composable
+fun OnboardingPreview() {
+    ComposeBasicsCodelabTheme {
+        OnboardingScreen(onContinueClicked = {}) // Do nothing on click
     }
 }
