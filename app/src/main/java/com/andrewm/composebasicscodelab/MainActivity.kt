3823bdea53ddccfa277368d05d83d71c6d6cdc83
Andrew Marshall <andrewm@twitter.com>
2022-06-29 19:28:28 -0400

    Added extra state info

--- app/src/main/java/com/andrewm/composebasicscodelab/MainActivity.kt
+++ app/src/main/java/com/andrewm/composebasicscodelab/MainActivity.kt
@@ -3,13 +3,18 @@ package com.andrewm.composebasicscodelab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedButton
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
 import com.andrewm.composebasicscodelab.ui.theme.ComposeBasicsCodelabTheme
 
@@ -36,6 +41,10 @@ private fun MyApp(names: List<String> = listOf("World", "Android", "Everyone")) 
 
 @Composable
 fun Greeting(name: String) {
+
+    val expanded = remember { mutableStateOf(false) }
+    val extraPadding = if (expanded.value) 48.dp else 0.dp
+
     Surface(
         color = MaterialTheme.colors.primary,
         modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
@@ -43,14 +52,18 @@ fun Greeting(name: String) {
         Row(
             modifier = Modifier.padding(24.dp),
         ) {
-            Column(Modifier.weight(1f)) {
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(bottom = extraPadding)
+            ) {
                 Text(text = "Hello,")
                 Text(text = "$name!")
             }
 
             OutlinedButton(
-                onClick = { /*TODO*/ }) {
-                Text(text = "Show more")
+                onClick = { expanded.value = !expanded.value }) {
+                Text(text = if (expanded.value) "Show less" else "Show more")
             }
         }
 
